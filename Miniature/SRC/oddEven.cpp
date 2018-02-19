6a52a4026662745b99230efbaad4b29b6dff1411
Kundananji Sinkala <gussinku@student.gu.se>
2018-02-19 03:57:49 +0100

    intial set up of project directories and sample program files to test for oddEven numbers

---
+++ Miniature/SRC/oddEven.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <oddEvenChecker.hpp>
+
+int main(){
+    oddEvenChecker n;
+    std::cout<< "The  number is "<< n.isEven(5) << std::endl;
+    return 0;
+}
---
+++ Miniature/SRC/oddEvenChecker.hpp
@@ -0,0 +1,8 @@
+#ifndef ODDEVEN
+#define ODDEVEN
+#<sctdint>
+
+class OddEvenChecker {
+    public : 
+    bool isEven(uint16_t n);
+}

e270fe783433630e73d2191fe7b3139f5904d78f
Jonathan Partain <jonathan.partain@gmail.com>
2018-04-10 11:43:00 +0200

    added automation script on car, as well as for building and transferring docker images being built from the Dockerfile

--- src/automake-arm.sh
+++ src/automake-arm.sh
@@ -17,5 +17,32 @@ docker save autobuild:armhf > autobuild.tar &&
 echo "copy to beaglebone"
 scp autobuild.tar debian@192.168.8.1:~/ &&
 rm -f autobuild.tar &&
-ssh debian@192.168.8.1 -t 'sh autorun.sh'
 
+HELP=".HELP"
+
+if [ $# -eq 0 ]
+    then
+        # run soft as default
+        ssh debian@192.168.8.1 -t 'sh autorun-soft.sh'
+        exit 0
+fi
+
+
+# just run once
+for i in "$1"
+do
+    case $i in
+        -r|--run)
+            ssh debian@192.168.8.1 -t 'sh autorun-run.sh'
+        ;;
+        -s|--soft)
+            ssh debian@192.168.8.1 -t 'sh autorun-soft.sh'
+        ;;
+        *)
+        #invalid arg
+            echo "invalid argument: '$i'"
+            cat $HELP
+            exit 1
+        ;;
+    esac
+done

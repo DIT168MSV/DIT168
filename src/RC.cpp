ea4906d12eabab161332eb8e40cacc53dd192d2c
Kundananji Sinkala <gussinku@student.gu.se>
2018-04-16 22:14:25 +0200

    added dynamic Acceleration,deccelaration and user input menu

--- src/RC.cpp
+++ src/RC.cpp
@@ -3,40 +3,82 @@
 #include "cluon/Envelope.hpp"
 #include "messages.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     cluon::OD4Session od4(192, [](cluon::data::Envelope &&envelope) noexcept {});
 
-      while (1) {
+    float value{0};
+    unsigned char choice;
+
+    std::cout << "  Group 8 RemoteControl " << std::endl;
+    std::cout << "                       " << std::endl;
+    std::cout << "    Keys to use        " << std::endl;
+    std::cout << "           w           " << std::endl;
+    std::cout << "    a             d    " << std::endl;
+    std::cout << "           s           " << std::endl;
+    std::cout << "                       " << std::endl;
+
+
+    while (1) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
         opendlv::proxy::GroundSteeringReading msgSteering;
         opendlv::proxy::PedalPositionReading msgPedal;
 
-        std::string choice;
-        std::getline(std::cin, choice);
-        
-            switch (choice.at(0)) {
-                case 'w':   msgPedal.percent(0.5);
-                            msgSteering.steeringAngle(0);   
-                            break;
-                case 'a':   msgPedal.percent(0.25);
-                            msgSteering.steeringAngle(45);  
-                            break;
-                case 's':   msgPedal.percent(0);
-                            msgSteering.steeringAngle(0);  
-                            break;
-                case 'd':   msgPedal.percent(0.25);
-                            msgSteering.steeringAngle(-45);
-                            break;
-                default:    msgPedal.percent(0);
-                            msgSteering.steeringAngle(0);
-            }
 
-            std::cout << "Sending pedalPosition: " << msgPedal.percent() << std::endl;
-            od4.send(msgPedal);
+        std::cin >> choice;
 
-            std::cout << "Sending steeringAngle: " <<msgSteering.steeringAngle() << std::endl;
-            od4.send(msgSteering);
+        switch (choice) {
+            case 'w':
+                msgSteering.steeringAngle(0.0);
+                if (value <= 0.4) {
+                    value += 0.05;
+                } else {
+                    std::cout << "full speed reached" << std::endl;
+                }
+                msgPedal.percent(value);
+                break;
+
+            case 'a':
+                if (value >= 0.25) {
+                    value = 0.15;
+                }
+                msgPedal.percent(value);
+                msgSteering.steeringAngle(45);
+                break;
+
+            case 's':
+                if (value >= 0.1) {
+                    value = 0;
+                }
+                msgPedal.percent(value);
+                msgSteering.steeringAngle(0);
+                break;
+
+            case 'd':
+                if (value >= 0.25) {
+                    value = 0.15;
+                }
+                msgPedal.percent(value);
+                msgSteering.steeringAngle(-45);
+                break;
+            case 'r':
+                if (value >= 0.15) {
+                    value -= 0.05;
+                }
+                msgPedal.percent(value);
+                msgSteering.steeringAngle(0.0);
+                break;
+
+            default:
+                msgPedal.percent(value);
+                msgSteering.steeringAngle(0);
         }
-
-       return 0;
+        std::cout << "Sending pedalPosition :" << value * 100 << "%." << std::endl;
+        od4.send(msgPedal);
+        std::cout << "Sending steeringAngle: " << msgSteering.steeringAngle() << std::endl;
+        od4.send(msgSteering);
     }
+
+    return 0;
+}
